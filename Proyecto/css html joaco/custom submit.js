bfecd49ae73ec7c7c1e8e9d3b2f125f808f680c0
DwekEzequiel <dwekechu@gmail.com>
2023-09-06 15:48:44 -0300

    probanod cosas nuevas

--- Proyecto/css html joaco/custom submit.js
+++ Proyecto/css html joaco/custom submit.js
@@ -1,15 +1,9 @@
-function validarFormulario() {
-    var errors = [];
 
 
-
-    if (errors.length > 0) {
-        var errorContainer = document.getElementById("errorMessages");
-        errorContainer.innerHTML = errors.join("<br>");
-        errorContainer.style.color = "red";
-        return false;
-    }
-
-    window.location.href = "envio_exitoso.html";
-    return true;
+if(true){
+    let ritmo = document.getElementById(ritmo).value; 
+}
+constVariablesFormulario={
+ ritmo:ritmoValue,
+ 
 }
