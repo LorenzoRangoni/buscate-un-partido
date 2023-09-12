bcb801751727e82f8c7e4660c7d3d24223d07ff4
nico99987485 <141357581+nico99987485@users.noreply.github.com>
2023-09-12 10:26:14 -0300

    avance con login y db

---
+++ Basededatos/Registrarte.php
@@ -0,0 +1,34 @@
+<?php
+
+requiere 'conexion_db.php';
+
+if(isset($_POST['Registrarte'])) {
+
+    // Obtengo los valores enviados por el formulario
+    $mail=  $_POST['Mail'];
+    $nombre=  $_POST['Nombre'];
+    $contrasena= $_POST['password'];
+
+    // Insertamos los datos en la base de datos
+    $sql= "INSERT INTO jugadores (mail_registrado, nombre_registrado, contrasena_jugador) VALUES ('$mail', ' $nombre', '$contrasena')"; 
+    $resultado= mysqli_query ($conexion, $sql);
+
+    //inserccion correcta
+
+    if ($resultado) 
+    {
+        echo "datos enviados con exito";
+    }
+   
+    // inserccion fallida
+     else {
+        echo "No se puede insertar la informacion"."<br>";
+        echo "Error: " .$sql."<br>" mysqli_error($conexion); I
+
+     }
+    
+    
+    
+    
+}
+?>
--- Basededatos/conexion_db.php
+++ Basededatos/conexion_db.php
@@ -1,6 +1,8 @@
-<?php
-$conex= mysqli_connect ("localhost","jia4hgeiw0xrsz4kpjpm","pscale_ pw_9OrPORrnYforaotmp602Ala7vUOAir2qj8oVHto6WZS","buscateunpartido");
+<!-- <?php -->
+$conex= mysqli_connect ("aws.connect.psdb.cloud","jia4hgeiw0xrsz4kpjpm","pscale_ pw_9OrPORrnYforaotmp602Ala7vUOAir2qj8oVHto6WZS","buscateunpartido");
 
-
+if ($mysqli->connect_error) {
+    die("Error de conexión: " . $mysqli->connect_error);
+}
 
     ?>
