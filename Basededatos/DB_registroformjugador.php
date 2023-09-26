dae208a289ec42a9bff6af25e8252f3e75ab90ee
nico99987485 <141357581+nico99987485@users.noreply.github.com>
2023-09-26 09:38:58 -0300

    .

--- Basededatos/DB_registroformjugador.php
+++ Basededatos/DB_registroformjugador.php
@@ -19,9 +19,8 @@ $habilidad = $_POST["habilidad"];
 $sql = "INSERT INTO jugadores (nombre, apellido, altura, peso, edad, sub, disponibilidad_horaria_jugador, posicion_jugador, numero_de_telefono_jugador, habilidad) VALUES ('$nombre','$apellido' '$altura', '$peso', '$edad', '$sub', '$disponibilidad', '$posicion', '$telefono', '$habilidad')";
 
 if ($mysqli->query($sql) === TRUE) {
-    // Redirigir después de la inserción exitosa
     header("Location: http://localhost/buscate_un_partido/buscate-un-partido/Proyecto/css%20html%20joaco/");
-    exit();  // Asegura que el script se detenga después de la redirección
+    exit();  
 } else {
     echo "Error al insertar datos: " . $mysqli->error;
 }
