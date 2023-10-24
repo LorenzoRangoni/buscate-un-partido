6c8d500481bc61ca672f776db365fc8e8753cfa7
nico99987485 <141357581+nico99987485@users.noreply.github.com>
2023-10-24 08:57:04 -0300

    .

--- Basededatos/conexion_db.php
+++ Basededatos/conexion_db.php
@@ -1,6 +1,11 @@
 <?php
+/*
   $_ENV= parse_ini_file ('.env');
   $mysqli = mysqli_init();
   $mysqli->ssl_set(NULL, NULL, "../cacert.pem", NULL, NULL);
   $mysqli->real_connect($_ENV["HOST"], $_ENV["USERNAME"], $_ENV["PASSWORD"], $_ENV["DATABASE"]);
+*/
+$mysqli=new mysqli("localhost","root","","buscateunpartido");
+
+
 ?>
