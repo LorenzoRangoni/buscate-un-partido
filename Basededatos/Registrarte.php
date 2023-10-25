474465272506f9c91bb240d91bfadcf669e4359a
nico99987485 <141357581+nico99987485@users.noreply.github.com>
2023-10-25 14:59:14 -0300

    anda

--- Basededatos/Registrarte.php
+++ Basededatos/Registrarte.php
@@ -2,7 +2,8 @@
 
 include ("conexion_db.php");
 
-if(isset($_POST['Registrarte'])) {
+var_dump($_POST);
+
 
     // Obtengo los valores enviados por el formulario
     $mail=  $_POST['Mail'];
@@ -11,19 +12,19 @@ if(isset($_POST['Registrarte'])) {
 
     // Insertamos los datos en la base de datos
     $sql= "INSERT INTO jugadores (mail_registrado_login, nombre_registrado_login, contrasena_jugador) VALUES ('$mail', ' $nombre', '$contrasena')"; 
-    $resultado= mysqli_query ($conexion, $sql);
+    $resultado= $mysqli->query($sql);
 
     //inserccion correcta
+
+    echo $resultado;
 
     if ($resultado) 
     {
         
-        header ("Location: http://localhost/buscate_un_partido/buscate-un-partido/Proyecto/index.html");
+        header("Location: http://localhost/buscate_un_partido/buscate-un-partido/Proyecto/css%20html%20joaco/");
         exit();
     }
-   
-    // inserccion fallida
-     else {
+        else {
         echo "No se puede insertar la informacion"."<br>";
         echo "Error: " .$sql. mysqli_error($conexion);
      }
@@ -31,5 +32,4 @@ if(isset($_POST['Registrarte'])) {
     
     
     
-}
 ?>
