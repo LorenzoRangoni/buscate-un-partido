128609406996eb2ae7a8cce258e6ae6da2ccd181
nico99987485 <141357581+nico99987485@users.noreply.github.com>
2023-11-01 09:45:57 -0300

    zona

--- Basededatos/DB_registroformjugador.php
+++ Basededatos/DB_registroformjugador.php
@@ -7,6 +7,7 @@ include("conexion_db.php");
 $nombre = $_POST["nombre"];
 $apellido = $_POST["apellido"];
 $altura = $_POST["altura"];
+$zona = $_POST["zona"];
 $peso = $_POST["peso"];
 $edad = $_POST["edad"];
 $sub = $_POST["sub"];
@@ -16,7 +17,7 @@ $telefono = $_POST["telefono"];
 $habilidad = $_POST["habilidad"];
 
 // Consulta SQL para insertar datos
-$sql = "INSERT INTO jugadores (nombre, apellido, altura, peso, edad, sub, disponibilidad_horaria_jugador, posicion_jugador, numero_de_telefono_jugador, habilidad) VALUES ('$nombre','$apellido',$altura, '$peso', '$edad', '$sub', '$disponibilidad', '$posicion', '$telefono', '$habilidad')";
+$sql = "INSERT INTO jugadores (nombre, apellido, altura, zona_residencial, peso, edad, sub, disponibilidad_horaria_jugador, posicion_jugador, numero_de_telefono_jugador, habilidad) VALUES ('$nombre','$apellido',$altura,'$zona', '$peso', '$edad', '$sub', '$disponibilidad', '$posicion', '$telefono', '$habilidad')";
 
 if ($mysqli->query($sql) === TRUE) {
     header("Location: http://localhost/buscate_un_partido/buscate-un-partido/Proyecto/css%20html%20joaco/");
