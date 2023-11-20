9306712bc1290677d97cacbb37ae0e9e2dacb1c7
Lorenzo Rangoni <lolorangoni07@gmail.com>
2023-11-20 00:25:17 -0300

    css panel usuario cambiado

--- Proyecto/css html joaco/historialform.php
+++ Proyecto/css html joaco/historialform.php
@@ -1,55 +1,145 @@
-<?php session_start();
-   if (isset($_SESSION['user_id'])) {
-      $user_id = $_SESSION['user_id'];
-      $username = $_SESSION['username'];
-    
-   } else {
-       $user_id = "";
-       $username = "";
-   }
-   ?>
-
+<?php
+session_start();
+if (isset($_SESSION['user_id'])) {
+    $user_id = $_SESSION['user_id'];
+    $username = $_SESSION['username'];
+} else {
+    $user_id = "";
+    $username = "";
+}
+?>
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Historial de Formularios</title>
-    <link rel="stylesheet" href="styles8.css">
+    <link rel="stylesheet" href="styles6.css">
+    <nav class="nav container">
+        <div class="nav__logo">
+            <h2 class="nav__title">Partido</h2>
+        </div>
+        <div class="nav__links">
+            <a href="index.html" class="button">Inicio</a>
+            <a href="Pagina_de_registroylogin.html" class="button">Logearse</a>
+            <a href="encontrarjugadores.html" class="button">Encontrar jugador</a>
+            <a href="registrarse2.html" class="button">Formulario</a>
+ 
+    </div>
+
+    </nav>
+    <style>
+        body {
+            background-color: #3498db;
+            color: black; /* Texto en negro */
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        h1 {
+            text-align: center;
+            color: white;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        th, td {
+            padding: 12px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+
+        th {
+            background-color: #333; /* Color de fondo del encabezado */
+            color: white;
+        }
+
+        a {
+            color: #FFD700; /* Color dorado para los enlaces */
+            text-decoration: none;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+
+        /* Estilos para el popup */
+        .popup {
+            display: none;
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            padding: 20px;
+            background-color: #fff;
+            border: 1px solid #ccc;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            z-index: 1000;
+        }
+
+        /* Estilos para la superposición de fondo */
+        .overlay {
+            display: none;
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.5);
+            z-index: 999;
+        }
+
+        /* Estilos para el filtro */
+        .filtro {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+
+        /* Estilos para el botón de fecha */
+        .boton-fecha {
+            font-size: 18px;
+            padding: 10px;
+            display: block;
+            margin: 0 auto;
+        }
+    </style>
 </head>
+
 <body>
     <h1>Historial de Formularios Enviados</h1>
 
+    <!-- Filtro para ordenar por fecha -->
+    <div class="filtro">
+        <label for="ordenar">Ordenar por:</label>
+        <select id="ordenar" onchange="ordenarFormularios()">
+            <option value="asc">Más Antiguos</option>
+            <option value="desc">Más Recientes</option>
+        </select>
+    </div>
+
     <?php
-   include("../../Basededatos/conexion_db.php");
-   
-    
-    $sql= "SELECT * FROM formulario_jugadores WHERE id_jugador = '$user_id'";
+    include("../../Basededatos/conexion_db.php");
+
+    $orden = isset($_GET['orden']) && ($_GET['orden'] === 'asc' || $_GET['orden'] === 'desc') ? $_GET['orden'] : 'desc';
+    $sql = "SELECT * FROM formulario_jugadores WHERE id_jugador = '$user_id' ORDER BY fecha_registro_formulario_jugador $orden";
     $resultado = $mysqli->query($sql);
 
-    if ($resultado->num_rows > 0) {
+    if ($resultado && $resultado->num_rows > 0) {
         echo '<table>';
-        echo '<tr><th>Fecha de Envío</th><th>Nombre</th><th>Altura</th><th>Peso</th><th>Posicion</th><th>Teléfono</th><th>Disponibilidad horaria</th><th>Correo</th><th>Edad</th><th>sub</th><th>Habilidad</th><th>Zona residencial</th>';
+        echo '<tr><th>Fecha de Envío</th><th>Posición</th><th>Acciones</th></tr>';
 
         while ($row = $resultado->fetch_assoc()) {
             echo '<tr>';
-            echo '<td>' . $row["fecha_registro_formulario_jugador"] . '</td>';
-            echo '<td>' . $row["nombre"] . ' ' . $row["apellido"] . '</td>';
-            echo '<td>' . $row["altura"] . '</td>';
-            echo '<td>' . $row["peso"] . '</td>';
+            echo '<td><button class="boton-fecha" onclick="mostrarPopup(\'' . $row["fecha_registro_formulario_jugador"] . '\', \'' . $row["posicion_jugador"] . '\', \'' . $row["nombre"] . '\', \'' . $row["altura"] . '\', \'' . $row["peso"] . '\', \'' . $row["numero_de_telefono_jugador"] . '\', \'' . $row["disponibilidad_horaria_jugador"] . '\', \'' . $row["mail_del_jugador"] . '\', \'' . $row["edad"] . '\', \'' . $row["sub"] . '\', \'' . $row["habilidad"] . '\', \'' . $row["zona_residencial"] . '\')">' . $row["fecha_registro_formulario_jugador"] . '</button></td>';
             echo '<td>' . $row["posicion_jugador"] . '</td>';
-            echo '<td>' . $row["numero_de_telefono_jugador"] . '</td>';
-            echo '<td>' . $row["disponibilidad_horaria_jugador"] . '</td>';
-            echo '<td>' . $row["mail_del_jugador"] . '</td>';
-            echo '<td>' . $row["edad"] . '</td>';
-            echo '<td>' . $row["sub"] . '</td>';
-            echo '<td>' . $row["habilidad"] . '</td>';
-            echo '<td>' . $row["zona_residencial"] . '</td>';
-            echo '<td><a href="registrarse2_edicion.php" id=' . $row["id_formulario"] . '">Editar</a></td>';
-            echo '<td><a href="eliminarformulario.php" id=' . $row["id_formulario"] . ')">Eliminar</button></td>';
-            echo '</tr>';
-            
+            echo '<td><a href="registrarse2_edicion.php?id=' . $row["id_formulario"] . '">Editar</a> | <a href="eliminarformulario.php?id=' . $row["id_formulario"] . '">Eliminar</a></td>';
             echo '</tr>';
         }
         echo '</table>';
@@ -57,5 +147,51 @@
         echo 'No se encontraron registros en el historial.';
     }
     ?>
+
+    <!-- Popup y superposición de fondo -->
+    <div class="overlay" id="overlay" onclick="cerrarPopup()"></div>
+    <div class="popup" id="popup"></div>
+
+    <script>
+        function mostrarPopup(fecha, posicion, nombre, altura, peso, telefono, disponibilidad, correo, edad, sub, habilidad, zona) {
+            document.getElementById("overlay").style.display = "block";
+            document.getElementById("popup").style.display = "block";
+            document.getElementById("popup").innerHTML =
+                '<p>Fecha de Envío: ' + fecha + '</p>' +
+                '<p>Posición: ' + posicion + '</p>' +
+                '<p>Nombre: ' + nombre + '</p>' +
+                '<p>Altura: ' + altura + '</p>' +
+                '<p>Peso: ' + peso + '</p>' +
+                '<p>Teléfono: ' + telefono + '</p>' +
+                '<p>Disponibilidad horaria: ' + disponibilidad + '</p>' +
+                '<p>Correo: ' + correo + '</p>' +
+                '<p>Edad: ' + edad + '</p>' +
+                '<p>Sub: ' + sub + '</p>' +
+                '<p>Habilidad: ' + habilidad + '</p>' +
+                '<p>Zona Residencial: ' + zona + '</p>' +
+                '<button onclick="cerrarPopup()">Cerrar</button>';
+        }
+
+        function cerrarPopup() {
+            document.getElementById("overlay").style.display = "none";
+            document.getElementById("popup").style.display = "none";
+        }
+
+        function ordenarFormularios() {
+            // Obtener el valor seleccionado del filtro
+            var orden = document.getElementById("ordenar").value;
+            // Obtener la URL actual y quitar cualquier parámetro de orden existente
+            var url = window.location.href.split('?')[0];
+            // Redirigir a la URL actual con el nuevo parámetro de orden
+            window.location.href = url + '?orden=' + orden;
+        }
+
+        // Función para inicializar el valor correcto del filtro
+        window.onload = function () {
+            var ordenActual = '<?php echo isset($_GET['orden']) ? $_GET['orden'] : 'desc'; ?>';
+            document.getElementById('ordenar').value = ordenActual;
+        };
+    </script>
 </body>
+
 </html>
--- Proyecto/css html joaco/panelusuario.php
+++ Proyecto/css html joaco/panelusuario.php
@@ -9,31 +9,177 @@ if (isset($_SESSION['user_id'])) {
     $usuario = mysqli_fetch_assoc($resultado);
     $nombre = $usuario["nombre_registrado_login"];
     $mail = $usuario["mail_registrado_login"];
-    $contrasena= $usuario["contrasena_jugador"];
-
- } else {
-     $user_id = "";
-     $username = "";
- }
- 
-?> 
+    $contrasena = $usuario["contrasena_jugador"];
+} else {
+    $user_id = "";
+    $username = "";
+}
+?>
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="panelusuario.css">
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #3498db; /* Fondo azul similar al segundo CSS */
+            color: white;
+        }
+
+        header {
+            background-color: #2c3e50; /* Azul más oscuro para el encabezado */
+            color: white;
+            padding: 10px 0;
+            
+        }
+
+        .navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 20px;
+}
+
+.logo h1 {
+    margin: 0;
+    font-size: 24px;
+    color: white;
+}
+
+        .nav__links .button {
+            background-color: #3498db;
+            color: white;
+            border: 2px solid white;
+            border-radius: 5px;
+            padding: 10px 15px;
+            text-decoration: none;
+            transition: background-color 0.3s ease;
+        }
+
+        .nav__links .button:hover {
+            background-color: white;
+            color: #3498db;
+        }
+
+        .container {
+            max-width: 1200px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #3498db;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        .user-profile {
+            background-color: #2c3e50;
+            color: white;
+            padding: 20px;
+            border-radius: 5px;
+            margin-top: 20px;
+        }
+
+        .data-item {
+            display: flex;
+            align-items: center;
+            margin-bottom: 15px;
+        }
+
+        .data-label {
+            font-weight: bold;
+            margin-right: 10px;
+        }
+
+        .edit-button {
+            background-color: #3498db;
+            color: white;
+            border: none;
+            padding: 8px 12px;
+            font-size: 14px;
+            cursor: pointer;
+            border-radius: 5px;
+            text-decoration: none;
+            transition: background-color 0.3s;
+            margin-left: 10px;
+        }
+
+        .edit-button:hover {
+            background-color: #2980b9;
+        }
+
+        .modal-content {
+            background-color: #f2f2f2;
+            color: #333;
+            margin: 10% auto;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+            width: 80%;
+        }
+
+        .form-group {
+            margin-bottom: 20px;
+        }
+
+        form input[type="text"],
+        form input[type="email"],
+        form input[type="password"] {
+            width: calc(100% - 20px);
+            padding: 10px;
+            font-size: 16px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+
+        form button.save-button {
+            background-color: #3498db;
+            color: white;
+            border: none;
+            padding: 10px 20px;
+            font-size: 16px;
+            cursor: pointer;
+            border-radius: 5px;
+            transition: background-color 0.3s;
+        }
+
+        form button.save-button:hover {
+            background-color: #2980b9;
+        }
+
+        .nav__links {
+    display: flex;
+    align-items: center;
+    list-style: none;
+}
+    </style>
     <title>Buscate un partido</title>
 </head>
+
 <body>
-    <header>
+    <header class="headercop">
         <nav class="navbar">
             <div class="logo">
                 <h1>Fútbol App</h1>
+                <nav class="nav container">
+        <div class="nav__logo">
+        </div>
+        <div class="nav__links">
+            <a href="index.html" class="button">Inicio</a>
+            <a href="Pagina_de_registroylogin.html" class="button">Logearse</a>
+            <a href="encontrarjugadores.html" class="button">Encontrar jugador</a>
+            <a href="registrarse2.html" class="button">Formulario</a>
+ 
+    </div>
+
+    </nav>
             </div>
-            <a href="index.php">Volver al inicio</a>
-            <a href="http://localhost/buscate_un_partido/buscate-un-partido/Basededatos/CerrarSesion.php">Cerrar Sesión</a>
+            <a href="index.php" class="button">Volver al inicio</a>
+            <a href="http://localhost/buscate_un_partido/buscate-un-partido/Basededatos/CerrarSesion.php" class="button">Cerrar Sesión</a>
         </nav>
     </header>
 
@@ -53,7 +199,7 @@ if (isset($_SESSION['user_id'])) {
                 </div>
                 <div class="data-item">
                     <span class="data-label">Contraseña:</span>
-                    <span id="password">********</span>
+                    <span id="password">**</span>
                     <a href="#" class="edit-button" id="editPasswordButton">Editar</a>
                 </div>
             </div>
@@ -75,49 +221,41 @@ if (isset($_SESSION['user_id'])) {
     </div>
 
     <div id="editEmailModal" class="modal">
-    <div class="modal-content">
-        <span class="close" id="closeEmailModal">&times;</span>
-        <h2>Editar Correo Electrónico</h2>
-        <form id="editEmailForm" action="micuentaemail.php" method="post">
-         <!-- Agrega el atributo 'action' -->
-            <div class="form-group">
-                <label for="newEmail">Nuevo Correo Electrónico:</label>
-                <input type="email" id="newEmail" name="newEmail" required>
-            </div>
-            <button type="submit" class="save-button">Guardar Cambios</button>
-        </form>
+        <div class="modal-content">
+            <span class="close" id="closeEmailModal">&times;</span>
+            <h2>Editar Correo Electrónico</h2>
+            <form id="editEmailForm" action="micuentaemail.php" method="post">
+                <div class="form-group">
+                    <label for="newEmail">Nuevo Correo Electrónico:</label>
+                    <input type="email" id="newEmail" name="newEmail" required>
+                </div>
+                <button type="submit" class="save-button">Guardar Cambios</button>
+            </form>
+        </div>
     </div>
-</div>
 
-<div id="editPasswordModal"  class="modal">
-    <div class="modal-content">
-         <span class="close" id="closePasswordModal">&times;</span>
-    <h2>Editar Contrasena</h2>
-        <form id="editPasswordForm" action="micuentapassword_equipo.php" method="post">
-    <div class="form-group">
-        <label for="currentPassword">Contraseña Actual:</label>
-        <input type="password" id="currentPassword" name="currentPassword" required>
+    <div id="editPasswordModal" class="modal">
+        <div class="modal-content">
+            <span class="close" id="closePasswordModal">&times;</span>
+            <h2>Editar Contraseña</h2>
+            <form id="editPasswordForm" action="micuentapassword_equipo.php" method="post">
+                <div class="form-group">
+                    <label for="currentPassword">Contraseña Actual:</label>
+                    <input type="password" id="currentPassword" name="currentPassword" required>
+                </div>
+                <div class="form-group">
+                    <label for="newPassword">Nueva Contraseña:</label>
+                    <input type="password" id="newPassword" name="newPassword" required>
+                </div>
+                <div id="mensajealerta"></div>
+                <button type="submit" class="save-button">Guardar Cambios</button>
+            </form>
+        </div>
     </div>
-    <div class="form-group">
-        <label for="newPassword">Nueva Contraseña:</label>
-        <input type="password" id="newPassword" name="newPassword" required>
-    </div>
-    <div id="mensajealerta"> </div>
-    <button type="submit" class="save-button">Guardar Cambios</button>
-</form>
-    </div>
-</div>
 
-
-  
-
-
-<script src="editeemail.js"></script>
-<script src="panelusuario.js"></script>
-<script src="editepassword.js"></script>
-
-
-
-    
+    <script src="editeemail.js"></script>
+    <script src="panelusuario.js"></script>
+    <script src="editepassword.js"></script>
 </body>
+
 </html>
